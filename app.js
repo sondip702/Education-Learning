6a8737b6b7cafd98c5af778905a73b28d30cdc65
sondip dey <adrtadsx@gmail.com>
2025-11-23 02:08:27 +0600

    new file:   .env 	new file:   app.d.ts 	new file:   app.d.ts.map 	new file:   app.js 	new file:   app.js.map 	modified:   app.ts 	new file:   database/db.d.ts 	new file:   database/db.d.ts.map 	new file:   database/db.js 	new file:   database/db.js.map 	new file:   database/db.ts 	new file:   nodemon.json 	modified:   package-lock.json 	modified:   package.json 	new file:   server.d.ts 	new file:   server.d.ts.map 	new file:   server.js 	new file:   server.js.map 	new file:   server.ts 	new file:   tsconfig.json

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import express from "express";
-
 export const app = express();
-
 app.set("port", process.env.PORT || 3000);
-
+//# sourceMappingURL=app.js.map
---
+++ server.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { app } from "./app.js";
+
+const PORT = app.get("port");
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
